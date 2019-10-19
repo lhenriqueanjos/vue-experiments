fcc68aef77f0c8594230464cdf02429355758e9d
Luiz Henrique dos Anjos <lhenriqueanjos@gmail.com>
2019-10-19 20:36:16 -0300

    refactor: componente para o experiments thumbnail

---
+++ src/home/components/ExperimentThumbnail.vue
@@ -0,0 +1,59 @@
+<template>
+  <article class="experiment-thumbnail">
+    <router-link
+      :to="experimentRoute"
+      class="experiment-link"
+    >
+      <h2>{{ title }}</h2>
+
+      <p>{{ description }}</p>
+    </router-link>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'ExperimentThumbnail',
+  props: {
+    description: {
+      type: String,
+      required: true,
+    },
+    experimentRoute: {
+      type: Object,
+      default: () => ({ name: 'home' }),
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.experiment-thumbnail {
+  padding: 8px;
+
+  border-radius: 4px;
+  border: 1px solid #ccc;
+
+  margin-bottom: 8px;
+
+  width: 100%;
+
+  .experiment-link {
+    text-decoration: none;
+    color: unset;
+  }
+
+  h2 {
+    line-height: 1em;
+    margin: 8px 0;
+  }
+
+  p {
+    margin: 8px 0;
+  }
+}
+</style>
--- src/home/views/Home.vue
+++ src/home/views/Home.vue
@@ -5,57 +5,38 @@
     </div>
 
     <div class="main-content">
-      <div class="experiment-thumbnail">
-        <router-link
-          :to="{ name: 'lazyLoadedView'}"
-          class="experiment-link"
-        >
-          <h2>😴 Lazy loaded route</h2>
+      <ExperimentThumbnail
+        title="😴 Lazy loaded route"
+        description="Como utilizar a feature de split do bundle para
+          postergar o carregamento de uma rota."
+        :experiment-route="{ name: 'lazyLoadedView'}"
+      />
 
-          <p>
-            Como utilizar a feature de split do bundle para
-            postergar o carregamento de uma rota.
-          </p>
-        </router-link>
-      </div>
+      <ExperimentThumbnail
+        title="🔬 Scoped slots"
+        description="Exemplo de componente DataProvider utilizando
+          scoped slots."
+        :experiment-route="{ name: 'scopedSlots'}"
+      />
 
-      <div class="experiment-thumbnail">
-        <router-link
-          :to="{ name: 'scopedSlots'}"
-          class="experiment-link"
-        >
-          <h2>🔬 Scoped slots</h2>
-
-          <p>
-            Exemplo de componente DataProvider utilizando
-            scoped slots.
-          </p>
-        </router-link>
-      </div>
-
-      <div class="experiment-thumbnail">
-        <router-link
-          :to="{ name: 'criticalCss'}"
-          class="experiment-link"
-        >
-          <h2>💋 Critical CSS</h2>
-
-          <p>
-            Usando o critter para colocar o CSS crítico
-            como inline e o restante como um arquivo.
-          </p>
-        </router-link>
-      </div>
+      <ExperimentThumbnail
+        title="💋 Critical CSS"
+        description="Usando o critter para colocar o CSS crítico
+            como inline e o restante como um arquivo."
+        :experiment-route="{ name: 'criticalCss'}"
+      />
     </div>
   </main>
 </template>
 
 <script>
 import { logoPng } from '@/assets';
+import ExperimentThumbnail from '../components/ExperimentThumbnail.vue';
 
 export default {
   name: 'Home',
   components: {
+    ExperimentThumbnail,
   },
   data() {
     return {
@@ -89,31 +70,6 @@ export default {
     align-items: flex-start;
 
     padding: 8px;
-
-    .experiment-thumbnail {
-      padding: 8px;
-
-      border-radius: 4px;
-      border: 1px solid #ccc;
-
-      margin-bottom: 8px;
-
-      width: 100%;
-
-      .experiment-link {
-        text-decoration: none;
-        color: unset;
-      }
-
-      h2 {
-        line-height: 1em;
-        margin: 8px 0;
-      }
-
-      p {
-        margin: 8px 0;
-      }
-    }
   }
 }
 </style>
